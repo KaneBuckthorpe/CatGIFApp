b4da6cdae4816bb8de98ecd37dc6b1930635fd77
kane Buckthorpe <kanebuckthorpe@gmail.com>
2022-11-21 19:15:30 +0000

    Created Xcode project

---
+++ CatGIFApp/CatGIFAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatGIFAppApp.swift
+//  CatGIFApp
+//
+//  Created by kane buckthorpe on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatGIFAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
